dcd0cea61f28047d911b1f2027e66eb0ada6239c
Oliver Abraham <mail@oliver-abraham.de>
2022-06-12 15:57:08 +0200

    cleanup

--- ProgramSettingsManagerDemo/Configuration.cs
+++ ProgramSettingsManagerDemo/Configuration.cs
@@ -4,5 +4,5 @@ internal class Configuration
 {
     public string Option1   { get; set; }
     public string Option2   { get; set; }
-    public string Option3	{ get; set; }
+    public string Option3   { get; set; }
 }
